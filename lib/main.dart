f9fe32242bc8f5b35c2329540f87fe69490fa162
ammarabid29 <aammarawan786@gmail.com>
2023-09-15 12:45:42 +0500

    model sheet

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:expense_tracker/widgets/expenses.dart';
 void main() {
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(useMaterial3: true),
       home: const Expenses(),
     ),
--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -23,6 +23,15 @@ class _ExpensesState extends State<Expenses> {
         category: Category.leisure),
   ];
 
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      context: context,
+      builder: (ctx) => const Text(
+        "Bottom Model Sheet",
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,7 +39,7 @@ class _ExpensesState extends State<Expenses> {
         title: const Text("Flutter ExpenseTracker"),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: _openAddExpenseOverlay,
             icon: const Icon(Icons.add),
           ),
         ],
